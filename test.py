e126e7e759395fc52d0225196689c0137784aaf0
halltech1 <78272578+halltech1@users.noreply.github.com>
2022-04-12 20:54:27 -0700

    This is a modified version

--- test.py
+++ test.py
@@ -5,3 +5,10 @@ Created on Tue Apr 12 20:35:54 2022
 @author: acer
 """
 print("Working with the github repository")
+check = input("Are you exited?: ")
+
+if check.title() == 'Yes':
+    print("Wow, that's really great")
+    
+else:
+    print("Please, how can we help you out")
